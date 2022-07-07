943ff4dc242cc6775686e92b6ee4425e7a72fa8d
Saint-Theana <Saint-Theana@github.com>
2022-07-08 01:14:03 +0800

    update

--- build.sh
+++ build.sh
@@ -51,6 +51,7 @@ make install
 
 cd ${ROOT_PATH}
 cd liblzma
+autogen
 PKG_CONFIG_PATH=${PKG_CONFIG_PATH} CFLAGS=${CFLAGS} CXXFLAGS=${CXXFLAGS} LDFLAGS=${LDFLAGS} CC=${CC} RANLIB=${RANLIB} CXX=${CXX} AR=${AR} LD=${LD} ./configure --prefix=${PREFIX} --host=${HOST} --target=${TARGET} --build=${BUILD} --enable-static
 make -j8
 make install
